f36356eb332df8270222b035441813a5f3cb1e79
YimSeMin <smyim428@gmail.com>
2023-01-21 03:05:07 +0900

    chore: 불필요한 함수 제거 - recorder.py

--- util/recorder.py
+++ util/recorder.py
@@ -3,45 +3,13 @@ import time
 from datetime import timedelta as _timedelta
 
 
-class TxtRecorder:
-    def __init__(self):
-        if sys.stdout != sys.__stdout__:
-            sys.stdout.close()
-            sys.stdout = sys.__stdout__
-        else:
-            pass
-
-        # self.original_sys_stdout = sys.__stdout__     # 기존 값을 저장해두는 방법
-        self.txt_name = sys.stdout.name
-        self.isRecording = False
-
-    def txt_record_end(self):
-        if self.isRecording:
-            sys.stdout.close()
-            sys.stdout = sys.__stdout__
-            print(self.txt_name, "에 출력결과를 기록 끝!")
-            self.isRecording = False
-        else:
-            print("기록 중이 아닙니다.")
-
-    def txt_record_start(self, filename='output.txt', how='w', encoding='utf-8'):
-        if self.isRecording:
-            self.txt_record_end()
-        else:
-            pass
-
-        print(filename, "에 출력결과를 기록 시작!")
-        sys.stdout = open(filename, how, encoding=encoding)
-        # sys.stdout.reconfigure(encoding='utf-8')  # python 3.7 이상
-        self.txt_name = sys.stdout.name
-        self.isRecording = True
-
-
 class WithTxtRecorder(object):
     """ with 구문 속 작업의 출력 결과 기록
+
     Example:
         with WithTxtRecorder('output.txt', 'w', 'utf-8') as recorder.sys.stdout:
             print(result)
+
     """
     def __init__(self, filename: str = 'output.txt', how: str = 'w', encoding: str = 'utf-8'):
         self.file_name = filename
@@ -57,33 +25,13 @@ class WithTxtRecorder(object):
         sys.stdout = sys.__stdout__
 
 
-class TimeRecorder:
-    def __init__(self):
-        self.record_paper = {}
-        self.the_time_when_this_start = time.time()
-
-    def time_record_start(self, entry: str = 'task1'):
-        print("[기록 시작]: ", entry)
-        self.record_paper[entry] = time.time()
-
-    def time_record_end(self, entry='task1'):
-        if entry in self.record_paper:
-            self.record_paper[entry + '_end'] = time.time()
-            print("[기록 끝]: ", entry, " 소요시간:",
-                  _timedelta(seconds=self.record_paper[entry+'_end'] - self.record_paper[entry]))
-        else:
-            print("[기록 끝]: ", entry, " 소요시간:",
-                  _timedelta(seconds=self.record_paper[entry+'_end'] - self.the_time_when_this_start))
-
-    def time_record_reset_all(self):
-        self.record_paper.clear()
-
-
 class WithTimeRecorder:
     """ with 구문 속 작업에 대한 시간 측정
+
     Example:
         with WithTimeRecorder('task1'):
             # do something
+
     """
     def __init__(self, entry: str = 'task1'):
         self.entry = entry
