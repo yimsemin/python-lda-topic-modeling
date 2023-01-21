2d0e9f2763d8c2e6e68926b457c849465e8b5a36
YimSeMin <smyim428@gmail.com>
2023-01-22 00:30:21 +0900

    Refactor: _setting()을 dict 으로 수정

--- lda_seq.py
+++ lda_seq.py
@@ -11,40 +11,46 @@ import util.recorder as recorder
 
 
 def _setting():
-    xlsx_name = "input/test.xlsx"
-    column_name = "article"
-    sheet_name = "preprocessed_result"
+    # input
+    input_data = {
+        'xlsx_name': 'input/test.xlsx',
+        'sheet_name': 'preprocessed',
+        'column_name': 'article',
+        'load_seq_model_dir': 'result_seq/',
 
-    load_seq_model_from: str = "result_seq/lda_seq_model"
-    save_result_directory = "result_seq/"
+        # LDA_시계열 셋팅 // 데이터가 시간순으로 정렬되어있어야 함.
+        'sheet_name_seq': "preprocessed",
+        'column_name_seq': "time"
+    }
 
-    num_topics: int = 7
+    # output
+    output_data = {
+        'result_dir': 'result_seq/',
+        'result_seq_model_dir': 'result_seq/',
+        'set_task': 1
+        # 1 = Hot and Cold
+        # 2 = 저장된 LDA_seq 모델을 불러와서 시각화하기
+    }
 
-    iterations: int = 20
-    random_state: int = 4190
+    # model setting
+    model_data = {
+        'num_topics': 7,
 
-    # LDA_시계열 셋팅 // 데이터가 시간순으로 정렬되어있어야 합니다.
-    column_name_seq = "time"
-    sheet_name_seq = "preprocessed_result"
+        # 모델 생성
+        'iterations': 20,
+        'random_state': 4190,
+    }
 
-    # 수행할 작업
-    set_task: int = 1
-    # 1 = LDA_seq 모델을 생성하고 저장하기
-    # 2 = 저장된 LDA_seq 모델을 불러와서 시각화하기
-
-    tokenized_article_series = openxlsx.load_series_from_xlsx(xlsx_name,
-                                                              column_name,
-                                                              sheet_name=sheet_name,
+    tokenized_article_series = openxlsx.load_series_from_xlsx(input_data['xlsx_name'],
+                                                              input_data['column_name'],
+                                                              input_data['sheet_name'],
                                                               is_list_in_list=True)
-
-    time_series = openxlsx.load_series_from_xlsx(xlsx_name, column_name_seq, sheet_name_seq)
+    time_series = openxlsx.load_series_from_xlsx(input_data['xlsx_name'],
+                                                 input_data['column_name_seq'],
+                                                 input_data['sheet_name_seq'])
     time_slice = time_series.value_counts(sort=False).tolist()
 
-    if set_task == 2:
-        lda_seq_model = LdaSeqModel.load(load_seq_model_from)
-
-    return tokenized_article_series, num_topics, iterations, random_state,\
-        save_result_directory, time_series, time_slice
+    return input_data, output_data, model_data, tokenized_article_series, time_series, time_slice
 
 
 def get_lda_seq_model(tokenized_article_series, time_slice, num_topics, random_state, iterations):
@@ -67,75 +73,78 @@ def get_lda_seq_model(tokenized_article_series, time_slice, num_topics, random_s
     return lda_seq_model
 
 
-def get_theta_for_each_article_each_topic(lda_seq_model, time_series):
-    # 각 문서별로 토픽 분포도 값 구하기
+def get_theta_for_each_article_each_topic(lda_seq_model, time_series) -> pd.DataFrame:
 
-    theta_value = lda_seq_model.doc_topics(0).tolist()
-    theta_values = pd.DataFrame(columns=['id'] + ['year'] + ['topic%d' % i for i in range(1, len(theta_value) + 1)])
-
-    # header만 있는 csv 파일 만들기
-    theta_values.to_csv('test/test.csv', index=False, mode='w', encoding='utf-8')
-
-    # 문서별 토픽 분포도 값 채워넣기
+    # {'아티클 번호: int': [yyyy, topic1_theta, topic2_theta, topic3_theta, ... ]}
+    theta_values = {}
     article_counter = 0
     while True:
         try:
-            print('%d번째 토픽~!' % article_counter)
             theta_value = lda_seq_model.doc_topics(article_counter).tolist()
-            theta_values.loc[0] = [article_counter, time_series[article_counter], *theta_value]
-            # To-Do
-            # id 와 year가 float로 뒤에 .0이 붙음
-            # 정수로 바꾸면 깔끔할텐데 방법을 알아보자
+            theta_values[f'{article_counter + 1}'] = [time_series[article_counter], *theta_value]
 
-            theta_values.to_csv('test/test.csv', index=False, mode='a', encoding='utf-8', header=False)
             article_counter += 1
         except IndexError:
             break
 
+    # DataFrame으로 만들기
+    header = ['year', *[f'topic{i}' for i in range(1, len(theta_values['1']))]]
+    result = pd.DataFrame.from_dict(theta_values, orient='index', columns=header)
 
-def results_summary_to_dataframe(results):
+    return result
+
+
+def get_linear_regression_results(reg_model) -> pd.DataFrame:
+    """
+
+    Args:
+        reg_model: sm.OLS.from_formula('y ~ x', data).fit()
+
+    Returns:
+        상수 제외 x -> y 의 주요 통계값들
+
+    """
     # https://stackoverflow.com/questions/51734180/converting-statsmodels-summary-object-to-pandas-dataframe
+    # 전체 목록을 보고 싶은 경우
+    # print(result.summary())
 
-    results_df = pd.DataFrame({"F_value": results.fvalue,
-                               "F_p_value": results.f_pvalue,
-                               "r_squared": results.rsquared,
-                               "co_eff": results.params,
-                               "std_err": results.bse,
-                               "t_value": results.tvalues,
-                               "p_value": results.pvalues,
-                               "conf_lower": results.conf_int()[0],
-                               "conf_higher": results.conf_int()[1]
+    reg_result = pd.DataFrame({"F_value": reg_model.fvalue,
+                               "F_p_value": reg_model.f_pvalue,
+                               "r_squared": reg_model.rsquared,
+                               "co_eff": reg_model.params,
+                               "std_err": reg_model.bse,
+                               "t_value": reg_model.tvalues,
+                               "p_value": reg_model.pvalues,
+                               "conf_lower": reg_model.conf_int()[0],
+                               "conf_higher": reg_model.conf_int()[1]
                                }
                               )
 
     # 상수(Intercept) 제외 후 결과 출력
-    return results_df.drop(['Intercept'])
+    return reg_result.drop(['Intercept'])
 
 
-def compute_each_topic_linear_regression(num_topics):
+def compute_each_topic_linear_regression(theta_df):
 
-    csv_df = pd.read_csv('test/test.csv')
+    # theta_df = pd.read_csv('test/test.csv')
 
-    results_df = pd.DataFrame(columns=["F_value", "F_p_value", "r_squared",
-                                       "co_eff", "std_err", "t_value", "p_value",
-                                       "conf_lower", "conf_higher"])
-    results_df.to_csv('test/test_reg_result.csv', mode='w', encoding='utf-8')
+    reg_results = pd.DataFrame()
     topic = 1
     while True:
         try:
-            reg_result = sm.OLS.from_formula("year ~ topic%d" % topic, csv_df).fit()
-            results_df = results_summary_to_dataframe(reg_result)
-            results_df.to_csv('test/test_reg_result.csv', mode='a', encoding='utf-8', header=False)
+            reg_model = sm.OLS.from_formula(f'year ~ topic{topic}', theta_df).fit()
+            reg_result = get_linear_regression_results(reg_model)
+            reg_results = pd.Concat(reg_results, reg_result)
 
             topic += 1
         except patsy.PatsyError:
             break
 
-    # 전체 목록을 보고 싶은 경우
-    # print(result.summary())
+    return reg_results
 
 
 def save_seq_topics_evolution_txt(lda_seq_model, num_topics):
+    # TODO
     with recorder.WithTxtRecorder as recorder.sys.stdout:
         for i in num_topics:
             print('[[---- %d 번 토픽 ----]]'%i)
@@ -144,6 +153,7 @@ def save_seq_topics_evolution_txt(lda_seq_model, num_topics):
 
 
 def save_topic_evolution_graph(df, save_graph_to: str = 'result/topic_evolution.png'):
+    # TODO
     fig, ax = plt.subplots(figsize=(30, 10))
     df.groupby(['period', 'topic_id'], sort=False).mean()['distribution'].unstack().plot(ax=ax,
                                                                                          grid=True,
@@ -163,29 +173,27 @@ def save_topic_evolution_graph(df, save_graph_to: str = 'result/topic_evolution.
     plt.clf()
 
 
-def save_lda_seq_model(lda_seq_model, save_to_here: str = 'result/lda_seq_model'):
-    lda_seq_model.save(save_to_here)
-
-
-def load_lda_seq_model(load_from_here: str = 'result/lda_seq_model'):
-    lda_seq_model = LdaSeqModel.load(load_from_here)
-
-    return lda_seq_model
-
-
 def main():
-    tokenized_article_series, num_topics, iterations, random_state, \
-        save_result_directory, time_series, time_slice = _setting()
+    # setting
+    _, output_data, model_data, tokenized_article_series, time_series, time_slice = _setting()
 
-    # lda_seq_model = get_lda_seq_model(tokenized_article_series, time_slice, num_topics, random_state, iterations)
+    iterations, random_state = model_data['iterations'], model_data['random_state']
 
-    # save_lda_seq_model(lda_seq_model, 'result_seq/test')
+    # LDA seq modeling
+    try:
+        lda_seq_model = LdaSeqModel.load(output_data['result_model_dir']
+                                         + f'lda_k_{model_data["num_topics"]}_rd_{random_state}')
+    except FileNotFoundError:
+        lda_seq_model = get_lda_seq_model(tokenized_article_series, time_slice,
+                                          model_data['num_topics'], random_state, iterations)
+        lda_seq_model.save(output_data['result_model_dir'] + f'lda_k_{model_data["num_topics"]}_rd_{random_state}')
 
-    lda_seq_model = load_lda_seq_model('result_seq/test')
+    # Hot and Cold
+    theta_df = get_theta_for_each_article_each_topic(lda_seq_model, time_series)
+    theta_df.to_csv(output_data['result_dir'] + 'lda_seq_theta.csv', index=True, index_label='id', mode='w')
 
-    # get_theta_for_each_article_each_topic(lda_seq_model, time_series)
-
-    compute_each_topic_linear_regression(num_topics)
+    reg_results = compute_each_topic_linear_regression(theta_df)
+    reg_results.to_csv(output_data['result_dir'] + 'lda_seq_hot_and_cold.csv', index=True, index_label='id', mode='w')
 
 
 if __name__ == '__main__':
