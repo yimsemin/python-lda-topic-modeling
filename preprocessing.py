01d659416adc2a1223df6aabfa1cf62beb9f1377
YimSeMin <smyim428@gmail.com>
2023-01-24 18:13:02 +0900

    Feat: 적게 등장한 단어 제거

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from tqdm import tqdm
 
 from konlpy.tag import Okt
@@ -18,7 +19,8 @@ def _setting():
     output_data = {
         'result_xlsx_name': 'input/test.xlsx',
         'result_sheet_name': 'preprocessed',
-        'result_column_name': 'article'
+        'result_column_name': 'article',
+        'min_word_count': 50
     }
 
     return input_data, output_data
@@ -66,7 +68,7 @@ def remove_stop_words_from_each_article(article_series):
     return result
 
 
-def remove_one_character_from_each_article(article_series):
+def remove_one_character_from_each_article(article_series) -> pd.Series:
     """ series의 각 열에서 한 글자인 단어 제거
 
     Args:
@@ -85,16 +87,25 @@ def remove_one_character_from_each_article(article_series):
     return result
 
 
-def noun(xlsx_name: str = 'test.xlsx', sheet_name=None, column_name: str = 'article'):
+def remove_low_count_word(tokenized_article_series, min_word_count: int = 20) -> pd.Series:
+
+    word_count_series = tokenized_article_series.explode().value_counts()
+    word_to_delete = word_count_series[word_count_series <= min_word_count].index.tolist()
+
+    return pd.Series([[i for i in article if i not in word_to_delete] for article in tokenized_article_series])
+
+
+def noun(xlsx_name: str = 'test.xlsx', sheet_name=None, column_name: str = 'article', min_word_count: int = 50):
     """
 
     Args:
-        xlsx_name: input
-        sheet_name: input
-        column_name: input
+        xlsx_name:
+        sheet_name:
+        column_name:
+        min_word_count:
 
     Returns:
-        series: 명사 추출 -> 불용어 제거 -> 한글자 제거
+        series: 명사 추출 -> 불용어 제거 -> 한글자 제거 -> 적게 등장한 글자 제거
 
     """
     if sheet_name is None:
@@ -104,6 +115,7 @@ def noun(xlsx_name: str = 'test.xlsx', sheet_name=None, column_name: str = 'arti
     tokenized_article_series = extract_noun_for_each_article(article_series)
     tokenized_article_series = remove_stop_words_from_each_article(tokenized_article_series)
     tokenized_article_series = remove_one_character_from_each_article(tokenized_article_series)
+    tokenized_article_series = remove_low_count_word(tokenized_article_series, min_word_count)
 
     return tokenized_article_series
 
@@ -114,7 +126,10 @@ def main():
     input_data, output_data = _setting()
 
     # preprocess - Noun
-    tokenized_article_series = noun(input_data['xlsx_name'], input_data['sheet_name'], input_data['column_name'])
+    tokenized_article_series = noun(input_data['xlsx_name'],
+                                    input_data['sheet_name'],
+                                    input_data['column_name'],
+                                    output_data['min_word_count'])
 
     # save result
     openxlsx.save_to_xlsx(tokenized_article_series,
